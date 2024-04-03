83a645edc2b667564757feda37ebb1f7a3e552df
Benjamin Fisico <BenjaminFisico@gmail.com>
2024-04-02 21:56:42 -0300

    Astro repository scope config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://benjaminfisico.github.io/My-portfolio/',
-    base: '/My-portfolio',
+    site: 'https://benjaminfisico.github.io/Que-sume-landingPage/',
+    base: '/Que-sume-landingPage',
 });
